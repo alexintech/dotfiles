c88c821a58112a2a5002ef76919b5c9181fc102a
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2022-09-21 12:58:50 +0600

    Changed setup of fzf configs

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
     echo -e "  \u001b[34;1m2) for Powerlevel10K\u001b[0m"
     echo -e "  \u001b[34;1m3) for Vundle\u001b[0m"
     echo -e "  \u001b[34;1m4) for Custom zsh plugins\u001b[0m"
+    echo -e "  \u001b[34;1mmac-arm64) for linking dotfiles for macOs ARM\u001b[0m"
+    echo -e "  \u001b[34;1mmac-intel) for linking dotfiles for macOs Intel\u001b[0m"
     echo -e "  \u001b[31;1m0) to Exit\u001b[0m"
 
     read option
@@ -39,6 +41,16 @@ if [[ "$arg" == "-i" || "$arg" == "--install" ]]; then
         cd $CWD
         ;;
 
+    "mac-arm64")echo -e "\u001b[7mLinking dotfiles for macOs ARM...\u001b[0m"
+        ln -sfnv $CWD/mac-arm64/.fzf.bash ~/.fzf.bash
+        ln -sfnv $CWD/mac-arm64/.fzf.zsh ~/.fzf.zsh
+        ;;
+
+    "mac-intel")echo -e "\u001b[7mLinking dotfiles for macOs Intel...\u001b[0m"
+        ln -sfnv $CWD/mac-intel/.fzf.bash ~/.fzf.bash
+        ln -sfnv $CWD/mac-intel/.fzf.zsh ~/.fzf.zsh
+        ;;
+
     "0")echo -e "\u001b[4m\u001b[44mBye\u001b[0m"
         exit 0
         ;;
@@ -58,8 +70,6 @@ mv -iv ~/.vimrc ~/.vimrc.old
 mv -iv ~/.tmux.conf ~/.tmux.conf.old
 mv -iv ~/.gitconfig ~/.gitconfig.old
 mv -iv ~/.gitignore_global ~/.gitignore_global.old
-mv -iv ~/.fzf.bash ~/.fzf.bash.old
-mv -iv ~/.fzf.zsh ~/.fzf.zsh.old
 
 echo -e "\u001b[36;1mAdding symlinks...\u001b[0m"
 ln -sfnv $CWD/.zshrc ~/.zshrc
@@ -68,7 +78,5 @@ ln -sfnv $CWD/.vimrc ~/.vimrc
 ln -sfnv $CWD/.tmux.conf ~/.tmux.conf
 ln -sfnv $CWD/.gitconfig ~/.gitconfig
 ln -sfnv $CWD/.gitignore_global ~/.gitignore_global
-ln -sfnv $CWD/.fzf.bash ~/.fzf.bash
-ln -sfnv $CWD/.fzf.zsh ~/.fzf.zsh
 
 echo -e "\u001b[32;1mDone.\u001b[0m"
